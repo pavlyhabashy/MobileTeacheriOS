29fca36a1662bda72cefedbf44b3a1a0d99fa902
Pavly Habashy <phabashy@usc.edu>
2019-09-20 14:54:16 -0700

    added pod stuff

--- MobileTeacher/Controller/Table View Cells/VideoTableViewCell.swift
+++ MobileTeacher/Controller/Table View Cells/VideoTableViewCell.swift
@@ -18,9 +18,15 @@ class VideoTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var ContainerView: UIView!
+    @IBOutlet weak var playButtonOutlet: UIButton!
     
     var videoItem: Video!
     var delegate: VideoCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
     
     func setVideo(video: Video) {
         videoItem = video
@@ -36,4 +42,22 @@ class VideoTableViewCell: UITableViewCell {
         delegate?.didTapShareButton(url: videoItem.url)
     }
     
+}
+
+extension UIButton {
+    func selectedButton(title:String, iconName: String, widthConstraints: NSLayoutConstraint){
+        self.backgroundColor = UIColor(red: 0, green: 118/255, blue: 254/255, alpha: 1)
+        self.setTitle(title, for: .normal)
+        self.setTitle(title, for: .highlighted)
+        self.setTitleColor(UIColor.white, for: .normal)
+        self.setTitleColor(UIColor.white, for: .highlighted)
+        self.setImage(UIImage(named: iconName), for: .normal)
+        self.setImage(UIImage(named: iconName), for: .highlighted)
+        let imageWidth = self.imageView!.frame.width
+        let textWidth = (title as NSString).size(withAttributes:[NSAttributedString.Key.font:self.titleLabel!.font!]).width
+        let width = textWidth + imageWidth + 24
+        //24 - the sum of your insets from left and right
+        widthConstraints.constant = width
+        self.layoutIfNeeded()
+    }
 }
