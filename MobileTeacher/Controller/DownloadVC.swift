25ae55f8930b10589972112101d50778897a0b92
Willie Shen <williesh@usc.edu>
2020-08-10 20:10:09 -0400

    Looks like able to download in background

--- MobileTeacher/Controller/DownloadVC.swift
+++ MobileTeacher/Controller/DownloadVC.swift
@@ -19,6 +19,7 @@ class DownloadVC: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     
     var video: Video!
+    var backgroundTask: UIBackgroundTaskIdentifier = UIBackgroundTaskIdentifier.invalid
     
     private var assetCollection: PHAssetCollection!
     var albumFound : Bool = false
@@ -34,6 +35,20 @@ class DownloadVC: UIViewController {
         titleLabel.text = video.title
         descriptionLabel.text = video.description
         // Do any additional setup after loading the view.
+    }
+    
+    //https://medium.com/@abhimuralidharan/finite-length-tasks-in-background-ios-swift-60f2db4fa01b
+    func registerBackgroundTask() {
+        backgroundTask = UIApplication.shared.beginBackgroundTask { [weak self] in
+            self?.endBackgroundTask()
+        }
+        assert(backgroundTask != UIBackgroundTaskIdentifier.invalid)
+    }
+    
+    func endBackgroundTask() {
+        print("Background task ended.")
+        UIApplication.shared.endBackgroundTask(backgroundTask)
+        backgroundTask = UIBackgroundTaskIdentifier.invalid
     }
     
     func createAlbum() {
@@ -125,29 +140,25 @@ class DownloadVC: UIViewController {
 
     @IBAction func downloadClick(_ sender: Any) {
         
-        //DispatchQueue.main.sync{
-         //   createAlbum()
-        //}
-        
+
         self.checkAuthorizationWithHandler(completion: {_ in
             
             print("Created Album Successfully")
         })
         
-        var array = video.downloadURL.absoluteString.components(separatedBy: "id=")
         
-        print(array)
-        //https://drive.google.com/open?id=1jlgGUrFWtDsGu8DQW5QiZGsm7v6rykB0
-        print(video.downloadURL.absoluteString)
-        var id = video.downloadURL.absoluteString.dropFirst("https://drive.google.com/open?id=".count)
-        //https://www.hackingwithswift.com/example-code/strings/how-to-remove-a-prefix-from-a-string
+        let concurrentQueue = DispatchQueue(label: "swiftlee.concurrent.queue", attributes: .concurrent)
+
         
-        //print(id)
+        //let storage = Storage.storage()
+        //let pathReference = storage.reference(withPath: video.storage)
         
-        let storage = Storage.storage()
-        let pathReference = storage.reference(withPath: video.storage)
+        //https://www.avanderlee.com/swift/concurrent-serial-dispatchqueue/
+        //https://stackoverflow.com/questions/24056205/how-to-use-background-thread-in-swift
         
-        var url = video.downloadURL
+        registerBackgroundTask()
+        DispatchQueue.global(qos: .background).async{
+            var url = self.video.downloadURL
 
         //pathReference.downloadURL{ url, error in
             var vid = AVAsset(url: url!)
@@ -156,12 +167,9 @@ class DownloadVC: UIViewController {
             let formatter = DateFormatter()
             formatter.dateFormat = "YYYY-MM-DD"
             
-            //vid.creationDate?.setValue(formatter.string(from:date), forKey: "value")
             
             print(vid.creationDate)
-            //print(vid.modificationDate)
-            //print(vid.isCompatibleWithSavedPhotosAlbum)
-            //print(UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(url!.absoluteString))
+
             
             let attributes = [
                 FileAttributeKey.creationDate: NSDate(),
@@ -170,9 +178,6 @@ class DownloadVC: UIViewController {
             
             print(NSDate())
            
-        
-         
-            
             let urlData = NSData(contentsOf: url!)
             
             
@@ -193,7 +198,7 @@ class DownloadVC: UIViewController {
                 
                 if let response = response as? HTTPURLResponse{
                     if response.statusCode == 200{
-                        DispatchQueue.main.async{
+                        DispatchQueue.global(qos: .background).async{
                             if let data = data{
                                 if let _ = try? data.write(to: destinationURL, options: Data.WritingOptions.atomic){
                                     print(destinationURL)
@@ -201,11 +206,6 @@ class DownloadVC: UIViewController {
                                     let preset = AVAssetExportPresetHighestQuality
                                     let outFileType = AVFileType.mov
                                     
-                                    /*do{
-                                        try FileManager.default.setAttributes(attributes, ofItemAtPath:destinationURL.path)
-                                    }catch{
-                                        print(error)
-                                    }*/
                                     
                                     AVAssetExportSession.determineCompatibility(ofExportPreset: preset,
                                                                                 with: vid, outputFileType: outFileType) { isCompatible in
@@ -224,7 +224,7 @@ class DownloadVC: UIViewController {
                                         print(UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(destinationURL.path))//https://medium.com/@Dougly/persisting-image-data-locally-swift-3-8bae72673f8a
                                         
                                         //https://www.tutorialspoint.com/how-to-download-a-video-using-a-url-and-save-it-in-an-photo-album-using-swift
-                                        DispatchQueue.main.async {
+                                        DispatchQueue.global(qos: .background).async {
                                             print("Downloading to gallery")
                                             PHPhotoLibrary.requestAuthorization({ (authorizationStatus: PHAuthorizationStatus) -> Void in
                                                 
@@ -272,183 +272,12 @@ class DownloadVC: UIViewController {
             }.resume()
             
 
-            
-            //print(UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(destinationURL.absoluteString))
-            
-            
-            /*
-            DispatchQueue.main.async {
-                print("Downloading to gallery")
-                PHPhotoLibrary.requestAuthorization({ (authorizationStatus: PHAuthorizationStatus) -> Void in
-                    
-                    if authorizationStatus == .authorized { //https://stackoverflow.com/questions/35503723/swift-downloading-video-with-downloadtaskwithurl
-                        urlData!.write(toFile:destinationURL.absoluteString, atomically: true) //https://stackoverflow.com/questions/39543214/declaring-url-in-swift-3/39546882
-                        PHPhotoLibrary.shared().performChanges({
-                            PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: destinationURL)
-                    
-                        }){ complete, error in
-                            if complete {
-                                print("Complete")
-                            }
-                    
-                            if error != nil{
-                                print(error)
-                            }
-                    
-                        }
-                    }
-                })
-                
-            }
-            */
-           /* var request = URLRequest(url: url!)
-            request.httpMethod = "GET"
-            
-            _ = URLSession.shared.dataTask(with: request){ data, response, error in
-                
-                if error != nil{
-                    print("Some error occured")
-                    return
-                }
-                
-                if let response = response as? HTTPURLResponse{
-                    if response.statusCode == 200{
-                        DispatchQueue.main.async{
-                            if let data = data{
-                                if let _ = try? data.write(to: destinationURL, options: Data.WritingOptions.atomic){
-                                    print(destinationURL)
-                                    print(UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(destinationURL.absoluteString))
-                                    
-                                    //let asset = AVAsset(url: destinationURL)
-                                    //let player = AVPlayer(url: destinationURL)
-                                    //let playerViewController = AVPlayerViewController()
-                                    
-                                    //playerViewController.player = player
-                                    //self.present(playerViewController, animated:true)
-                                    
-                                    
-                                }
-                            }else{
-                                print("Error")
-                            }
-                        }
-                    }
-                }
-                
-            }.resume()
-            */
-            
-            //first save the video into the file system
-            /*let task = URLSession.shared.downloadTask(with: url!) { localURL, urlResponse, error in
-                
-                
-
-                
-                print(urlResponse)
-                print(error)
-                
-                guard let fileURL = localURL else {return}
-                do{
-                    let documentsURL = try
-                        FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-                    let savedURL = documentsURL.appendingPathComponent(fileURL.lastPathComponent)
-                    print(savedURL)
-                    try FileManager.default.moveItem(at: fileURL, to: savedURL)
-                    
-                    print(UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(savedURL.absoluteString))
-                }catch {
-                    print("file error: \(error)")
-                }
-            }
-            
-             task.resume()
-            //then, save the video to the photo album*/
+        }
+        
         }
         
        
         
-        /*pathReference.downloadURL { url, error in
-            if let error = error{
-                print(error)
-            }else{
-                
-                let urlData = NSData(contentsOf: url!) ?? nil
-                print(urlData)
-                DispatchQueue.main.async {
-                    print("Trying to get vid")
-                    let pic = UIImage(data: urlData as! Data) //http://swiftdeveloperblog.com/code-examples/uiimageview-and-uiimage-load-image-from-remote-url/
 
-                }
-                print(pic)
-                UIImageWriteToSavedPhotosAlbum(pic!, nil, nil, nil)
-                //print(url)
-                /*let urlData = NSData(contentsOf: url!) ?? nil
-                
-                let galleryPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0];
-                
-                //https://www.tutorialspoint.com/how-to-download-a-video-using-a-url-and-save-it-in-an-photo-album-using-swift
-                
-                
-                //download to camera roll
-                let filePath="\(galleryPath)/\(UUID().uuidString).mp4"
-                
-                print(filePath)
-                PHPhotoLibrary.shared().performChanges({
-                    PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: url!) //https://stackoverflow.com/questions/29482738/swift-save-video-from-nsurl-to-user-camera-roll
-                    
-                }){ saved, error in
-                    if saved{
-                        print("Saved")
-                    }
-                    if error != nil{
-                        print(error)
-                    }
-                   
-                }*/
-                /*DispatchQueue.main.async {
-                    urlData?.write(toFile: filePath, atomically: true)
-                   PHPhotoLibrary.shared().performChanges({
-                   PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL:
-                   URL(fileURLWithPath: filePath))
-                }) {
-                   success, error in
-                   if success {
-                      print("Succesfully Saved")
-                   } else {
-                      print(error?.localizedDescription)
-                   }
-                }
-                
-            }*/
-                
-            
-        }
-        
-    }*/
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-//}
 }
 
-/*extension UIImageView {
-    func load(url:URL){
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url){
-                if let image = UIImage(data: data){
-                    DispatchQueue.main.async{
-                        self?.image = image
-                    }
-                }
-            }
-            
-        }
-    }
-}*/
